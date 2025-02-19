f5339ed9ad47cd744ec05d0cf029ecb7d131e787
7minutech <elijohnson1107@gmail.com>
2025-02-18 21:05:24 -0600

    feat: added id and methods
    Need to convert from event type id to punchtype
    in PunchDAO

--- src/edu/jsu/mcis/cs310/tas_sp25/EventType.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/EventType.java
@@ -2,19 +2,34 @@ package edu.jsu.mcis.cs310.tas_sp25;
 
 public enum EventType {
 
-    CLOCK_OUT("CLOCK OUT"),
-    CLOCK_IN("CLOCK IN"),
-    TIME_OUT("TIME OUT");
+    CLOCK_OUT(0,"CLOCK OUT"),
+    CLOCK_IN(1, "CLOCK IN"),
+    TIME_OUT(2,"TIME OUT");
 
     private final String description;
+    private final int eventTypeId;
 
-    private EventType(String d) {
+    private EventType(int id, String d) {
+        eventTypeId = id;
         description = d;
     }
 
     @Override
     public String toString() {
         return description;
+    }
+    
+    public int getId(){
+        return eventTypeId;
+    }
+    
+    public static EventType findById(int id){
+        for(EventType punchtype: EventType.values()){
+            if (punchtype.getId() == id){
+                return punchtype;
+            }
+        }
+        return null;
     }
 
 }
