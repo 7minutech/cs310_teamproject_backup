4b72ac831c1646f9ff3d8c11489f746ef729577e
mattthefox <ngraysonmatt@gmail.com>
2025-02-21 12:37:57 -0600

    New Shift tests.

--- test/edu/jsu/mcis/cs310/tas_sp25/ShiftFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_sp25/ShiftFindTest.java
@@ -31,6 +31,7 @@ public class ShiftFindTest {
         assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
         assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s2.toString());
         assertEquals("Shift 1 Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)", s3.toString());
+        // New Tests
 
     }
 
@@ -45,19 +46,36 @@ public class ShiftFindTest {
         Badge b1 = badgeDAO.find("B6902696");
         Badge b2 = badgeDAO.find("76E920D9");
         Badge b3 = badgeDAO.find("4382D92D");
-
+        // New tests
+        Badge b4 = badgeDAO.find("DD6E2C0C"); // shift 1
+        Badge b5 = badgeDAO.find("398B1563"); // shift 2
+        Badge b6 = badgeDAO.find("4382D92D"); // shift 3
+        
         /* Retrieve Shift Rulesets from Database */
         
         Shift s1 = shiftDAO.find(b1);
         Shift s2 = shiftDAO.find(b2);
         Shift s3 = shiftDAO.find(b3);
+        
+        Shift s4 = shiftDAO.find(b4);
+        Shift s5 = shiftDAO.find(b5);
+        Shift s6 = shiftDAO.find(b6);
 
         /* Compare to Expected Values */
         
         assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
         assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s2.toString());
         assertEquals("Shift 1 Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)", s3.toString());
+        
+        /* New Tests */
+        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s4.toString());
+        assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s5.toString());
+        assertEquals("Shift 1 Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)", s6.toString());
+        // Compare to Shift Propeties
+        assertEquals(15, s1.getRoundInterval());
+        assertEquals("07:00", s4.getShiftStart().toString());
+        assertEquals(5, s6.getGracePeriod());
+        
 
     }
-
 }
