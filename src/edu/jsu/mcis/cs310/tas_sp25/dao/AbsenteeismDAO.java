e8c06e447c535393ee32303183f16e9dcb01b51a
7minutech <elijohnson1107@gmail.com>
2025-03-12 08:44:40 -0500

    feat: added create method functionality
    Must create absent entry if not already and if
    there must update new absent percent b/c that's
    the only changing field

--- src/edu/jsu/mcis/cs310/tas_sp25/dao/AbsenteeismDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/dao/AbsenteeismDAO.java
@@ -14,6 +14,9 @@ public class AbsenteeismDAO {
     private static final String QUERY_FIND = "SELECT * "
                                             + "FROM absenteeism "
                                             + "WHERE employeeid = ?, payperiod = ?";
+    private static final String QUERY_CREATE = "INSERT INTO absenteeism (employeeid, payperiod, percentage) VALUES (?, ?, ?)"
+                                             + "ON DUPLICATE KEY UPDATE percentage = ?";
+
 
     private final DAOFactory daoFactory;
     private final EmployeeDAO employeeDAO;
@@ -75,5 +78,34 @@ public class AbsenteeismDAO {
          * it should be replaced with a new one to reflect the new absenteeism percentage.  
          * (Again, the pay period date added to the database should always be the date of the start of the pay period.)
          */
+        PreparedStatement ps = null;
+        
+        try {
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_CREATE);
+                LocalDate date = absent.getPayStart();
+                Date sqlDate = Date.valueOf(date);
+                double percentage = absent.getPercentage().doubleValue();
+                ps.setInt(1, absent.getEmployee().getId());
+                ps.setDate(2, sqlDate);
+                ps.setDouble(3, percentage);
+                ps.setDouble(4, percentage);
+                
+                ps.executeUpdate();
+            }
+
+            
+
+        } 
+        catch (Exception e) { e.printStackTrace(); }
+
+        finally {
+
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+
+        }
     }
 }
