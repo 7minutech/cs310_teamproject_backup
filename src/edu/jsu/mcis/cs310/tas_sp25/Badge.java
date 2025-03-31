6f13694f82a420433deef79e3d78d515611a4b16
katjuarez <kattyjuarez5@gmail.com>
2025-03-31 10:42:08 -0500

    feat: javadocs for Badge

--- src/edu/jsu/mcis/cs310/tas_sp25/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/Badge.java
@@ -1,22 +1,57 @@
 package edu.jsu.mcis.cs310.tas_sp25;
 
+/**
+ * <p>The {@code Badge} class represents an identification badge used by 
+ * employees in the time and attendance system. Each badge has a unique 
+ * ID and a textual description.</p>
+ * 
+ * <p>This class is typically associated with employees and used to 
+ * identify them when clocking in or out at a terminal.</p>
+ * 
+ * @author
+ */
 public class Badge {
-
+    
+    /**
+     * Identifier and description for the badge.
+     */
     private final String id, description;
-
+    
+    /**
+     * Constructs a {@code Badge} with a specific ID and description.
+     * 
+     * @param id The unique badge ID
+     * @param description A short description of the badge
+     */
     public Badge(String id, String description) {
         this.id = id;
         this.description = description;
     }
-
+    
+    /**
+     * Gets the unique ID of the badge.
+     * 
+     * @return the badge ID
+     */
     public String getId() {
         return id;
     }
-
+    
+    /**
+     * Gets the description of the badge.
+     * 
+     * @return the badge description
+     */
     public String getDescription() {
         return description;
     }
-
+    
+    /**
+     * Returns a string representation of the badge in the format:
+     * "#ID (Description)"
+     * 
+     * @return a formatted string showing the badge ID and description
+     */
     @Override
     public String toString() {
 
