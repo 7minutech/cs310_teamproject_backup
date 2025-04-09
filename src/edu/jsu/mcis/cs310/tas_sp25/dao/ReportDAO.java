c9c1075e95840cba1fc3d4947a695a93d353fe7a
7minutech <elijohnson1107@gmail.com>
2025-04-09 08:55:26 -0500

    feat: query for getWhosInWhosOut and partial work
    getWhosInWhosOut method has been worked on partial
    with the query and method return a json string
    in the wrong order and only getting employees
    who are in

--- src/edu/jsu/mcis/cs310/tas_sp25/dao/ReportDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/dao/ReportDAO.java
@@ -5,22 +5,24 @@ import com.github.cliftonlabs.json_simple.JsonObject;
 import edu.jsu.mcis.cs310.tas_sp25.*;
 import java.time.LocalDateTime;
 import java.sql.*;
-
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class ReportDAO {
 
      private static final String QUERY_FIND_EMPLOYEES_BY_DATE = 
-            "SELECT *" +
-            "FROM EMPLOYEE " +
-            "WHERE badgeid IN ( " +
-            "    SELECT EMP.badgeid " +
-            "    FROM EMPLOYEE AS EMP " +
-            "    JOIN EVENT AS EVT " +
-            "    ON EMP.badgeid = EVT.badgeid " +
-            "    WHERE EVT.timestamp BETWEEN ? AND ? " +
-            ") " +
-            "AND DEPARTMENTID = ? " +
-            "ORDER BY LASTNAME";
+            "SELECT EMP.FIRSTNAME, EMP.LASTNAME, EMP.badgeid, EVT.timestamp, EMPTYPE.DESCRIPTION, SHIFT.DESCRIPTION " +
+             "FROM EMPLOYEE AS EMP " +
+             "JOIN EVENT AS EVT " +
+             "ON EMP.badgeid = EVT.badgeid " +
+             "JOIN EMPLOYEETYPE AS EMPTYPE " +
+             "ON EMP.EMPLOYEETYPEID = EMPTYPE.ID " +
+             "JOIN SHIFT ON EMP.SHIFTID = SHIFT.ID " +
+             "WHERE EVT.timestamp BETWEEN DATE_SUB(?, INTERVAL 15 MINUTE) AND DATE_ADD(?, INTERVAL 15 MINUTE) " +
+             "AND EMP.DEPARTMENTID = ? " +
+             "ORDER BY EMPTYPE.DESCRIPTION, EMP.LASTNAME, EMP.FIRSTNAME;";
 
     /** DAO factory for managing database connections and cross-DAO access. */
     private final DAOFactory daoFactory;
@@ -86,6 +88,57 @@ public class ReportDAO {
         "Full-Time Employee\",\"firstname\":\"Lee\",\"badgeid\":\"639D4185\"
         ,\"shift\":\"Shift 1\",\"lastname\":\"Gaines\",\"status\":\"In\"}]
         */
+        Timestamp sqlTimestamp = Timestamp.valueOf(timestamp);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        JsonArray employees = new JsonArray();
+        StringBuilder employeesString = new StringBuilder();
+        
+        try {
+            Connection conn = daoFactory.getConnection();
+            if (conn.isClosed()) {
+                daoFactory.createConnection();
+                conn = daoFactory.getConnection();
+            }
+            if (conn.isValid(0)) {
+                ps = conn.prepareStatement(QUERY_FIND_EMPLOYEES_BY_DATE);
+                ps.setTimestamp(1, sqlTimestamp);
+                ps.setTimestamp(2, sqlTimestamp);
+                ps.setInt(3, departmentId);
+                boolean hasResults = ps.execute();
+                if (hasResults) {
+                    rs = ps.getResultSet();
+                    while (rs.next()) {  
+                        JsonObject employee = new JsonObject();
+                        String firstName = rs.getString("firstname");
+                        String lastName = rs.getString("lastname");
+                        String badgeId = rs.getString("badgeId");
+                        Timestamp fetchedTimestamp = rs.getTimestamp("timestamp");
+                        LocalDateTime dateTimeTimestamp = fetchedTimestamp.toLocalDateTime();
+                        String dayOfWeek = dateTimeTimestamp.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
+                        SimpleDateFormat targetFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                        String formattedDate = targetFormat.format(fetchedTimestamp);
+                        String shiftDescription = rs.getString("shift.description");
+                        String employeeType = rs.getString("EMPTYPE.DESCRIPTION");
+                        employee.put("firstname", firstName);
+                        employee.put("lastname", lastName);
+                        employee.put("badgeid", badgeId);
+                        employee.put("arrived", dayOfWeek.toUpperCase() + " " + formattedDate);
+                        employee.put("shift", shiftDescription);
+                        employee.put("employeetype", employeeType);
+                        employee.put("status", "In");
+                        employees.add(employee);
+                    }
+                    employeesString.append(employees.toJson());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) { try { rs.close(); } catch (Exception e) { e.printStackTrace(); } }
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+        }
+        return employeesString.toString();
         
     }
 }
