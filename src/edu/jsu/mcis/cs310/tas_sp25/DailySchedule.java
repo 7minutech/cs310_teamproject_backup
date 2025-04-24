1a90b0461a65a6b726d04aac870dc2e5c3aa6499
7minutech <elijohnson1107@gmail.com>
2025-04-24 08:58:19 -0500

    feat: javadocs for DailySchedule setter/getters

--- src/edu/jsu/mcis/cs310/tas_sp25/DailySchedule.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/DailySchedule.java
@@ -55,70 +55,171 @@ public class DailySchedule {
     }
     
     // Getters
+    /**
+     * Gets the round interval.
+     * 
+     * @return The round interval.
+     */
     public int getRoundInterval() {
         return this.roundinterval;
     }
+
+    /**
+     * Gets the grace period.
+     * 
+     * @return The grace period.
+     */
     public int getGracePeriod() {
         return this.graceperiod;
     }
+
+    /**
+     * Gets the dock penalty.
+     * 
+     * @return The dock penalty.
+     */
     public int getDockPenalty() {
         return this.dockpenalty;
     }
+
+    /**
+     * Gets the lunch threshold.
+     * 
+     * @return The lunch threshold.
+     */
     public int getLunchThreshold() {
         return this.lunchthreshold;
     }
+
+    /**
+     * Gets the start time of the shift.
+     * 
+     * @return The start time of the shift.
+     */
     public LocalTime getShiftStart() {
         return this.shiftstart;
     }
+
+    /**
+     * Gets the end time of the shift.
+     * 
+     * @return The end time of the shift.
+     */
     public LocalTime getShiftStop() {
         return this.shiftstop;
     }
+
+    /**
+     * Gets the start time of the lunch break.
+     * 
+     * @return The start time of the lunch break.
+     */
     public LocalTime getLunchStart() {
         return this.lunchstart;
     }
+
+    /**
+     * Gets the end time of the lunch break.
+     * 
+     * @return The end time of the lunch break.
+     */
     public LocalTime getLunchStop() {
         return this.lunchstop;
     }
+
+    /**
+     * Calculates and returns the duration of the lunch break in minutes.
+     * 
+     * @return The lunch duration in minutes.
+     */
     public int getLunchDuration() {
         return (int) java.time.Duration.between(lunchstart, lunchstop).toMinutes();
     }
 
+    /**
+     * Calculates and returns the total duration of the shift in minutes.
+     * 
+     * @return The shift duration in minutes.
+     */
     public int getShiftDuration() {
         return (int) java.time.Duration.between(shiftstart, shiftstop).toMinutes();
     }
-    
+
+
     // Setters
+
+    /**
+     * Sets the round interval.
+     * 
+     * @param value The round interval to set.
+     */
     public void setRoundInterval(int value) {
         this.roundinterval = value;
     }
 
+    /**
+     * Sets the grace period.
+     * 
+     * @param value The grace period to set.
+     */
     public void setGracePeriod(int value) {
         this.graceperiod = value;
     }
 
+    /**
+     * Sets the dock penalty.
+     * 
+     * @param value The dock penalty to set.
+     */
     public void setDockPenalty(int value) {
         this.dockpenalty = value;
     }
 
+    /**
+     * Sets the lunch threshold.
+     * 
+     * @param value The lunch threshold to set.
+     */
     public void setLunchThreshold(int value) {
         this.lunchthreshold = value;
     }
 
+    /**
+     * Sets the start time of the shift.
+     * 
+     * @param value The start time of the shift to set.
+     */
     public void setShiftStart(LocalTime value) {
         this.shiftstart = value;
     }
 
+    /**
+     * Sets the end time of the shift.
+     * 
+     * @param value The end time of the shift to set.
+     */
     public void setShiftStop(LocalTime value) {
         this.shiftstop = value;
     }
 
+    /**
+     * Sets the start time of the lunch break.
+     * 
+     * @param value The start time of the lunch break to set.
+     */
     public void setLunchStart(LocalTime value) {
         this.lunchstart = value;
     }
 
+    /**
+     * Sets the end time of the lunch break.
+     * 
+     * @param value The end time of the lunch break to set.
+     */
     public void setLunchStop(LocalTime value) {
         this.lunchstop = value;
     }
+
     
     /**
      * Returns a formatted string representation of the daily schedule, including shift
