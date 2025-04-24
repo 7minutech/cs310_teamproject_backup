81e07d8c6193961a80de564b3fffe38ac92b72e2
7minutech <elijohnson1107@gmail.com>
2025-04-24 09:06:05 -0500

    feat: javadocs for Shift getters/setters

--- src/edu/jsu/mcis/cs310/tas_sp25/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/Shift.java
@@ -74,73 +74,181 @@ public class Shift {
     }
     
     // Getters
+
+    /**
+     * Returns the round interval value from the default schedule.
+     * 
+     * @return The round interval in minutes.
+     */
     public int getRoundInterval() {
         return this.defaultschedule.getRoundInterval();
     }
+
+    /**
+     * Returns the grace period value from the default schedule.
+     * 
+     * @return The grace period in minutes.
+     */
     public int getGracePeriod() {
         return this.defaultschedule.getGracePeriod();
     }
+
+    /**
+     * Returns the dock penalty value from the default schedule.
+     * 
+     * @return The dock penalty in minutes.
+     */
     public int getDockPenalty() {
         return this.defaultschedule.getDockPenalty();
     }
+
+    /**
+     * Returns the lunch threshold value from the default schedule.
+     * 
+     * @return The lunch threshold in minutes.
+     */
     public int getLunchThreshold() {
         return this.defaultschedule.getLunchThreshold();
     }
+
+    /**
+     * Returns the start time of the shift from the default schedule.
+     * 
+     * @return The start time of the shift as a {@link LocalTime}.
+     */
     public LocalTime getShiftStart() {
         return this.defaultschedule.getShiftStart();
     }
+
+    /**
+     * Returns the stop time of the shift from the default schedule.
+     * 
+     * @return The stop time of the shift as a {@link LocalTime}.
+     */
     public LocalTime getShiftStop() {
         return this.defaultschedule.getShiftStop();
     }
+
+    /**
+     * Returns the start time of lunch from the default schedule.
+     * 
+     * @return The start time of lunch as a {@link LocalTime}.
+     */
     public LocalTime getLunchStart() {
         return this.defaultschedule.getLunchStart();
     }
+
+    /**
+     * Returns the stop time of lunch from the default schedule.
+     * 
+     * @return The stop time of lunch as a {@link LocalTime}.
+     */
     public LocalTime getLunchStop() {
         return this.defaultschedule.getLunchStop();
     }
+
+    /**
+     * Returns the lunch duration in minutes from the default schedule.
+     * 
+     * @return The lunch duration in minutes.
+     */
     public int getLunchDuration() {
         return this.defaultschedule.getLunchDuration();
     }
 
+    /**
+     * Returns the shift duration in minutes from the default schedule.
+     * 
+     * @return The shift duration in minutes.
+     */
     public int getShiftDuration() {
         return this.defaultschedule.getShiftDuration();
     }
+
+    /**
+     * Returns the default schedule.
+     * 
+     * @return The {@link DailySchedule} object representing the default schedule.
+     */
     public DailySchedule getDefaultSchedule() {
         return this.defaultschedule;
     }
-    
+
+
     // Setters
+
+    /**
+     * Sets the round interval value in the default schedule.
+     * 
+     * @param value The round interval to set, in minutes.
+     */
     public void setRoundInterval(int value) {
         this.defaultschedule.setRoundInterval(value);
     }
+
+    /**
+     * Sets the grace period value in the default schedule.
+     * 
+     * @param value The grace period to set, in minutes.
+     */
     public void setGracePeriod(int value) {
         this.defaultschedule.setGracePeriod(value);
     }
 
+    /**
+     * Sets the dock penalty value in the default schedule.
+     * 
+     * @param value The dock penalty to set, in minutes.
+     */
     public void setDockPenalty(int value) {
         this.defaultschedule.setDockPenalty(value);
     }
 
+    /**
+     * Sets the lunch threshold value in the default schedule.
+     * 
+     * @param value The lunch threshold to set, in minutes.
+     */
     public void setLunchThreshold(int value) {
         this.defaultschedule.setLunchThreshold(value);
     }
 
+    /**
+     * Sets the start time of the shift in the default schedule.
+     * 
+     * @param value The start time of the shift to set as a {@link LocalTime}.
+     */
     public void setShiftStart(LocalTime value) {
         this.defaultschedule.setShiftStart(value);
     }
 
+    /**
+     * Sets the stop time of the shift in the default schedule.
+     * 
+     * @param value The stop time of the shift to set as a {@link LocalTime}.
+     */
     public void setShiftStop(LocalTime value) {
         this.defaultschedule.setShiftStop(value);
     }
 
+    /**
+     * Sets the start time of lunch in the default schedule.
+     * 
+     * @param value The start time of lunch to set as a {@link LocalTime}.
+     */
     public void setLunchStart(LocalTime value) {
         this.defaultschedule.setLunchStart(value);
     }
 
+    /**
+     * Sets the stop time of lunch in the default schedule.
+     * 
+     * @param value The stop time of lunch to set as a {@link LocalTime}.
+     */
     public void setLunchStop(LocalTime value) {
         this.defaultschedule.setLunchStop(value);
     }
-    
+
     /**
      * Returns a string with the shift and lunch times in the format:
      * "ShiftName: HH:MM - HH:MM (X minutes); Lunch: HH:MM - HH:MM (Y minutes)"
